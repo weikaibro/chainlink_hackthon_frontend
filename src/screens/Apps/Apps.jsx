dda27a44ab478ebedbe01e28ac202c829801e119
Canfly <2017xiao06@gmail.com>
2024-05-21 15:36:52 +0800

    apps update

--- src/screens/Apps/Apps.jsx
+++ src/screens/Apps/Apps.jsx
@@ -4,50 +4,41 @@ import { Link } from 'react-router-dom';
 function Apps() {
   return (
     <div className="flex flex-wrap gap-3 justify-center">
-      <Link to="/weather">
+      <Link to="/exchange">
         <div className="avatar">
           <div className="w-16 mask mask-squircle">
             <img src="/assets/images/weather-app.jpg" alt="weather App" />
           </div>
         </div>
       </Link>
-      <Link to="/calculator">
+      <Link to="/mail">
         <div className="avatar">
           <div className="w-16 mask mask-squircle">
             <img src="/assets/images/calculator.png" alt="Calculator App" />
           </div>
         </div>
       </Link>
-      <Link to="/notes">
+      <Link to="/text">
         <div className="avatar">
           <div className="w-16 mask mask-squircle">
             <img src="/assets/images/notes.jpg" alt="Notes App" />
           </div>
         </div>
       </Link>
-      <Link to="/jokes">
+      <Link to="/news">
         <div className="avatar">
           <div className="w-16 mask mask-squircle">
             <img src="/assets/images/funny.jpg" alt="Jokes App" />
           </div>
         </div>
       </Link>
-      <Link to="/cryptoapp">
+      {/* <Link to="/cryptoapp">
         <div className="avatar">
           <div className="w-16 mask mask-squircle">
             <img src="/assets/images/cryptoapp.png" alt="Jokes App" />
           </div>
         </div>
-      </Link>
-      {/* //TODO: Add more apps here */}
-      {/* <Link to="/tictactoe">
-        <div className="avatar">
-          <div className="w-16 mask mask-squircle">
-            <img src="/assets/images/tiktak.jpg" alt="tictactoe App" />
-          </div>
-        </div>
       </Link> */}
-
     </div>
   );
 }
